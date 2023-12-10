690ab76c96648ed1a0a89b5f717b9296a047dcb8
Luke Przkwas <luke@covalence.io>
2023-12-10 11:08:46 -0600

    adding error handlers to server

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
+import { APIError } from './utils/apiError';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = process.env.NODE_ENV === 'development';
@@ -32,6 +33,21 @@ if (isProduction) {
 		res.sendFile('index.html', { root: 'public' });
 	});
 }
+
+app.use((req, res, next) => {
+	const error = new APIError(`Path ${req.originalUrl} not found`);
+	error.status = 404;
+	next(error);
+});
+
+app.use(
+	(error: APIError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+		console.error(error);
+		const status = error.status || 500;
+		const message = error.message || 'Internal server error';
+		return res.status(status).json({ error: { message, status } });
+	}
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/server/utils/apiError.ts
@@ -0,0 +1,8 @@
+export class APIError extends Error {
+	status: number;
+
+	constructor(message: string, status: number = 500) {
+		super(message);
+		this.status = status;
+	}
+}
